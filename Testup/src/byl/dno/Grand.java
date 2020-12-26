e96961f52023c9a031c57a719017e7e1a0ef3edc
A-Dururu <dzerunosantiesu@gmail.com>
2020-12-26 23:23:40 +0200

    update 1

---
+++ Testup/src/byl/dno/Grand.java
@@ -0,0 +1,8 @@
+package byl.dno;
+
+public class Grand {
+    public Grand() {
+        int jav = 11;
+        System.out.println("хотя нет в степени " + jav);
+    }
+}
---
+++ Testup/src/byl/dno/Main.java
@@ -0,0 +1,19 @@
+package byl.dno;
+
+public class Main {
+    static final int jav = 9;
+
+    public static void main(String[] args) {
+        new Grand();
+        int g = 10, h = 25;
+        System.out.println(g);
+        System.out.println(jav);
+        System.out.println("gfirf ndfhm d cntgtyb " + g);
+        System.out.println("паша говно в степени " + h);
+        g = 1000;
+        System.out.println("хотя нет в степени " + g);
+        System.out.println("хотя нет в степени " + jav);
+//паша го в палыча
+
+    }
+}
